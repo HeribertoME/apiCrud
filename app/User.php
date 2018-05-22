77c8b18df6a087a464d321a946ff8e1cb5b1587b
HeribertoME <hmelizarraraz@hotmail.com>
2018-05-22 14:12:45 -0500

    Se agrega softdeleting a la tabla

--- app/User.php
+++ app/User.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     const USUARIO_VERIFICADO = '1';
     const USUARIO_NO_VERIFICADO = '0';
@@ -15,7 +16,9 @@ class User extends Authenticatable
     const USUARIO_ADMINISTRADOR = 'true';
     const USUARIO_REGULAR = 'false';
 
-
+    protected $table = 'users';
+    protected $dates = ['deleted_at'];
+    
     /**
      * The attributes that are mass assignable.
      *
